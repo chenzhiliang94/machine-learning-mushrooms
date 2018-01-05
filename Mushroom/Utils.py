c15ba98872340a147aad1b5490679bfef649fd67
chenzhiliang94 <chenzhiliang94@gmail.com>
2018-01-05 22:33:06 +0800

    Update Utils.py
    Include a new boolean variable to indicate whether to drop a dummy variable

--- Mushroom/Utils.py
+++ Mushroom/Utils.py
@@ -5,16 +5,24 @@ Created on 2 Jan 2018
 '''
 import pandas as pd
 
-def convertToDummy(csv_String, result_Name):
-    data = pd.read_csv(csv_String) #read data
-    result = data[result_Name] #identify result column
-    
-    del data[result_Name] #remove result column
-    del data['veil-type']
-    data = pd.get_dummies(data) #convert all dependent variables to dummy variables
-    #del data['odor_n']
-    data.insert(loc=0, column = result_Name, value=result) #insert back result column
-    return data
+def convertToDummy(csv_String, result_Name, drop):
+    if (drop == False):
+        data = pd.read_csv(csv_String) #read data
+        result = data[result_Name] #identify result column
+        
+        del data[result_Name] #remove result column
+        data = pd.get_dummies(data) #convert all dependent variables to dummy variables
+        #del data['odor_n']
+        data.insert(loc=0, column = result_Name, value=result) #insert back result column
+        return data
+    else:
+        data = pd.read_csv(csv_String) #read data
+        result = data[result_Name] #identify result column
+        
+        del data[result_Name] #remove result column
+        data = pd.get_dummies(data, drop_first = True) #convert all dependent variables to dummy variables
+        data.insert(loc=0, column = result_Name, value=result) #insert back result column
+        return data
     
 
 def valueOf(index, anotherArray):
